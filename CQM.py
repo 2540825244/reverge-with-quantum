f97ef2825dcaf5a3b33e0b8582987c000bebe2ca
Qty <qty10@outlook.com>
2023-07-10 11:16:31 +0300

    fixed variance of sums

--- CQM.py
+++ CQM.py
@@ -7,14 +7,12 @@ from dimod.generators.constraints import combinations
 from dwave.system import LeapHybridCQMSampler
 import datetime
 
-qs = [Integer(lower_bound=1, upper_bound=11, label='x' + str(i)) for i in range(11)] #question space
+qs = [Integer(lower_bound=0, upper_bound=12, label='x' + str(i)) for i in range(11)] #question space
 possible_values = range(1, 12)
 
 cqm = ConstrainedQuadraticModel()
 
-cqm.set_objective(((qs[0] + qs[1] + qs[4])**2 + (qs[1] + qs[2] + qs[5])**2 + (qs[2] + qs[3] + qs[6])**2 + (qs[4] + qs[7] + qs[8])**2 + (qs[5] + qs[8] + qs[9])**2 + (qs[6] + qs[9] + qs[10])**2)/6 - ((qs[0] + qs[1] + qs[4]) + (qs[1] + qs[2] + qs[5]) + (qs[2] + qs[3] + qs[6]) + (qs[4] + qs[7] + qs[8]) + (qs[5] + qs[8] + qs[9]) + (qs[6] + qs[9] + qs[10]))**2/36 
-                  + 10 * ((qs[0] + qs[1] + qs[2] + qs[3] + qs[4] + qs[5] + qs[6] + qs[7] + qs[8] + qs[9] + qs[10])/11-6)**2 
-                  + 10 * ((qs[0]**2+qs[1]**2+qs[2]**2+qs[3]**2+qs[4]**2+qs[5]**2+qs[6]**2+qs[7]**2+qs[8]**2+qs[9]**2+qs[10]**2)/11 - (qs[0]+qs[1]+qs[2]+qs[3]+qs[4]+qs[5]+qs[6]+qs[7]+qs[8]+qs[9]+qs[10])**2/121-10)) #just the variance of the sums, simple, also added constraint from comparison as objective
+cqm.set_objective(((qs[0] + qs[1] + qs[4])**2 + (qs[1] + qs[2] + qs[5])**2 + (qs[2] + qs[3] + qs[6])**2 + (qs[4] + qs[7] + qs[8])**2 + (qs[5] + qs[8] + qs[9])**2 + (qs[6] + qs[9] + qs[10])**2)/6 - ((qs[0] + qs[1] + qs[4]) + (qs[1] + qs[2] + qs[5]) + (qs[2] + qs[3] + qs[6]) + (qs[4] + qs[7] + qs[8]) + (qs[5] + qs[8] + qs[9]) + (qs[6] + qs[9] + qs[10]))**2/36) #just the variance of the sums, simple, also added constraint from comparison as objective
 
 # cqm.add_constraint(qs[0] + qs[1] + qs[4] == qs[1] + qs[2] + qs[5])
 # cqm.add_constraint(qs[1] + qs[2] + qs[5] == qs[2] + qs[3] + qs[6])
@@ -22,8 +20,8 @@ cqm.set_objective(((qs[0] + qs[1] + qs[4])**2 + (qs[1] + qs[2] + qs[5])**2 + (qs
 # cqm.add_constraint(qs[5] + qs[8] + qs[9] == qs[6] + qs[9] + qs[10])
 
 #constraint to make sure every variable is unique, using mean and variance
-# cqm.add_constraint_from_comparison((qs[0] + qs[1] + qs[2] + qs[3] + qs[4] + qs[5] + qs[6] + qs[7] + qs[8] + qs[9] + qs[10])/11 == 6)
-# cqm.add_constraint_from_comparison((qs[0]**2+qs[1]**2+qs[2]**2+qs[3]**2+qs[4]**2+qs[5]**2+qs[6]**2+qs[7]**2+qs[8]**2+qs[9]**2+qs[10]**2)/11 - (qs[0]+qs[1]+qs[2]+qs[3]+qs[4]+qs[5]+qs[6]+qs[7]+qs[8]+qs[9]+qs[10])**2/121 == 10)
+cqm.add_constraint_from_comparison((qs[0] + qs[1] + qs[2] + qs[3] + qs[4] + qs[5] + qs[6] + qs[7] + qs[8] + qs[9] + qs[10])/11 == 6)
+cqm.add_constraint_from_comparison((qs[0]**2+qs[1]**2+qs[2]**2+qs[3]**2+qs[4]**2+qs[5]**2+qs[6]**2+qs[7]**2+qs[8]**2+qs[9]**2+qs[10]**2)/11 - (qs[0]+qs[1]+qs[2]+qs[3]+qs[4]+qs[5]+qs[6]+qs[7]+qs[8]+qs[9]+qs[10])**2/121 == 10)
 
 #constraint pre-set values
 cqm.add_constraint(qs[0] == 6)
@@ -35,12 +33,14 @@ print("Solving...")
 sampler = LeapHybridCQMSampler()
 start = datetime.datetime.now()
 sampleset = sampler.sample_cqm(cqm)
+feasible_sampleset = sampleset.filter(lambda d: d.is_feasible)
+num_feasible = len(feasible_sampleset)
+best_samples = \
+    feasible_sampleset.truncate(min(10, num_feasible))
 end = datetime.datetime.now()
 print(f"Time used: {end - start}")
-print(sampleset.first.sample)
-print(sampleset.first.energy)
 
 #store results
-results = sampleset.first.sample
+print(best_samples)
 f_output = open(f"output.txt", "w")
-f_output.write(str(results))
+f_output.write(str(best_samples))
